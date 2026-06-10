b6f726c5eb4836288cd881b1818283360a83d522
Komediruzecki <komediruzecki.2015@gmail.com>
2026-06-10 23:27:50 +0000

    fix: update jest config for consolidated test/ directory

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>', '../tests'],
-  testMatch: ['../test/e2e/backend-api/**/*.spec.js'],
+  roots: ['<rootDir>', '../test'],
+  testMatch: ['**/e2e/backend-api/**/*.spec.js'],
   collectCoverageFrom: ['**/*.js', '!node_modules/**', '!index.js'],
   setupFilesAfterEnv: ['../test/jest.setup.js'],
   verbose: true,
   transformIgnorePatterns: ['node_modules/(?!(chai)/)'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': '<rootDir>/../tests/__mocks__/styleMock.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/../test/__mocks__/styleMock.js',
   },
 };
